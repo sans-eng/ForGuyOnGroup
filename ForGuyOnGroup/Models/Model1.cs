e7de2d89a4fb8d0f0a50ee8d8fbf9d204df1a125
Setya Adi Kurnia <engineering.ins@weq-tech.com>
2022-05-28 00:05:09 +0700

    Add project files.

---
+++ ForGuyOnGroup/Models/Model1.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForGuyOnGroup.Models
+{
+    [ObservableObject]
+    public partial class Model1
+    {
+        #region Private fields
+        [ObservableProperty]
+        private string _stringObject;
+        [ObservableProperty]
+        private ObservableCollection<string> _strings;
+        #endregion Private fields
+
+        #region Constructors
+        public Model1(string stringObject, ObservableCollection<string> strings)
+        {
+            _stringObject = stringObject;
+            _strings = strings;
+        }
+        #endregion Constructors
+    }
+}
---
+++ ForGuyOnGroup/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ForGuyOnGroup.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForGuyOnGroup.ViewModels
+{
+    [ObservableObject]
+    public partial class MainViewModel
+    {
+        #region Constructors
+        public MainViewModel()
+        {
+            Models.Add(new Model1("Item1", new ObservableCollection<string> {"Item 1 collection 1", "Item 1 collection 2", "Item 1 collection 3" }));
+            Models.Add(new Model1("Item2", new ObservableCollection<string> {"Item 2 collection 1", "Item 2 collection 2", "Item 2 collection 3" }));
+            Models.Add(new Model1("Item3", new ObservableCollection<string> {"Item 3 collection 1", "Item 3 collection 2", "Item 3 collection 3" }));
+            Models.Add(new Model1("Item4", new ObservableCollection<string> {"Item 4 collection 1", "Item 4 collection 2", "Item 4 collection 3" }));
+        }
+        #endregion Constructors
+        #region Public properties
+        public ObservableCollection<Model1> Models { get;} = new ObservableCollection<Model1>();
+        #endregion Public properties
+    }
+}
